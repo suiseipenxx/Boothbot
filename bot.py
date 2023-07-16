afb345a6046f6eaefe7b928e9660df921e8a3cd2
suiseipenxx <pandorabox57042@gmail.com>
2023-07-16 10:44:44 +0800

    優化運行和加載速度，並且讓程式在後台運行

--- bot.py
+++ bot.py
@@ -4,58 +4,72 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from retrying import retry
+import sys
 
 #輸入區
-url=""#搶購商品網址
-account=""#pixiv賬號
-password=""#pixiv密碼
+url="https://booth.pm/zh-tw/items/2535379"#搶購商品網址
+account="pandorabox57042@gmail.com"#pixiv賬號
+password="hyh010308"#pixiv密碼
 
-#優化程式
+#優化
+@retry(wait_fixed=2, stop_max_attempt_number=1)
+def send(path,ap):
+    driver.find_element(By.CSS_SELECTOR,path).send_keys(ap)
+def click(path):
+    driver.find_element(By.CSS_SELECTOR,path).click()
+
+#瀏覽器選項
 options = Options()
+options.page_load_strategy='none'
+options.add_argument('--headless')#無頭模式
+options.add_argument('--window-size=1920,1080')#設定視窗大小，開啟無頭模式情況下一定要有，不然會抓不到元素
+options.add_argument('--start-maximized')
+options.add_argument('--log-level=2')#設定log等級，不然會有很多log顯示
 options.add_argument('--no-sandbox')#最高權限
-#options.add_argument('--headless')#不顯示瀏覽器和執行過程，Linux不支援可視化，所以一定要加
 options.add_argument('--disable-javascript')#禁用JavaScript
-options.add_argument('blink-settings=imagesEnabled=false')#不加載圖片
+options.add_argument('--blink-settings=imagesEnabled=false')#不加載圖片
 options.add_argument('--disable-software-rasterizer')#禁用渲染
 options.add_argument('--disable-notifications')#禁用通知
 options.add_argument('--disk-cache-dir=/path/to/cache')#讀取瀏覽器暫存
 options.add_argument('--disable-gpu')#禁用使用內顯
-
-options.add_argument('utf-8')#設定語言和編碼方式
-
+options.add_argument("--disable-extensions")
+options.add_argument("start-maximized")
+options.add_argument("disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
 
 
 driver = webdriver.Chrome(options)
 driver.maximize_window()
 
+
 #操作部分
 driver.get(url)#搶購商品頁面 Purchasing Product Page
-
-
-driver.find_element(By.XPATH,'/html/body/header/div/div/nav/div[2]/div[1]/a').click()# 到登入頁面Redirecting to the login page
-
-driver.find_element(By.XPATH,'/html/body/div[2]/main/div[2]/div/div[1]/a').click()#登入pixiv Login to pixiv
-
+driver.implicitly_wait(2)#隱形等待
+click('[href="/users/sign_in"]')
+click('[class="btn pixiv-oauth signin full-length"]')
+print('登入中')
 #pixiv登入資料
-driver.find_element(By.XPATH,'//*[@id="app-mount-point"]/div/div[3]/div[1]/div[3]/div/div/div/form/fieldset[1]/label/input').send_keys(account) # 輸入帳號
-driver.find_element(By.XPATH,'//*[@id="app-mount-point"]/div/div[3]/div[1]/div[3]/div/div/div/form/fieldset[2]/label/input').send_keys(password) # 輸入密碼
-driver.find_element(By.XPATH,'//*[@id="app-mount-point"]/div/div[3]/div[1]/div[3]/div/div/div/form/button').click()
+driver.implicitly_wait(2)#隱形等待
+send('[class="sc-bn9ph6-6 degQSE"]',account)
+send('[class="sc-bn9ph6-6 hfoSmp"]',password)
+click('[class="sc-bdnxRM jvCTkj sc-dlnjwi klNrDe sc-2o1uwj-6 NmyKg sc-2o1uwj-6 NmyKg"]')
+print('登入成功')
 
-time.sleep(2)
-
+locators=(By.CSS_SELECTOR,'[class="btn add-cart full-length"]')#等待的條件
+print('搶商品中')
 while 1:
     try:
-        buy = WebDriverWait(driver, 1, 0.1).until(EC.presence_of_element_located((By.CSS_SELECTOR,"div[class='cmd-label']"))) # 顯性等待
+        buy = WebDriverWait(driver, 0.5, 0.1).until(EC.presence_of_element_located(locators)) # 顯性等待
         buy.click() # 偵測到可以購買按鈕就點擊按鈕
-        print ("可以購買!")
-        driver.find_element(By.XPATH,'//*[@id="js-carts-index"]/div[1]/div/div[4]/a[1]').click()#結賬付款
-        driver.find_element(By.XPATH,'//*[@id="new_order"]/div[2]/label[1]/div/div/div[3]/div/input').click()#確認支付方式
-        driver.find_element(By.XPATH,'//*[@id="persist"]/div/form/div/button').click()#確認收貨地點
-        driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/main/div/div[6]/form/div/input').click()#確認訂單 
+        click('[class="btn btn--primary u-w-sp-100"]')
+        click('[name="commit"]')
+        click('[name="button"]')
+        #click('[name="commit"]')
         print ("購買完成")
-        break
+        driver.quit()
+        sys.exit("結束程式")
     except:
-        print("還不能購買! 重新整理!")
         driver.refresh() # 重整頁面
 
 
